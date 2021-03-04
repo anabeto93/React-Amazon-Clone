1ce976ebe2b3a3e609e95b63f5c80734887c8f91
Richard Anabeto Opoku <anabeto93@gmail.com>
2021-03-04 11:27:33 +0000

    initial setup of checkout page with total amount computations and basket item count.

--- src/Home.js
+++ src/Home.js
@@ -14,12 +14,14 @@ function Home() {
 
                 <div className="home__row">
                     <Product 
+                        id={12341234}
                         title='The lean startup. The full product description' 
                         price={19.99} 
                         image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg"
                         rating={5}
                     />
                     <Product
+                        id={23451234}
                         title="Light switch"
                         price={20.15}
                         image="https://m.media-amazon.com/images/I/310bMKQjT5L._AC_SY200_.jpg"
@@ -28,18 +30,21 @@ function Home() {
                 </div>
                 <div className="home__row">
                     <Product 
+                        id={34561234}
                         title="Iphone"
                         price={449.99}
                         image="https://m.media-amazon.com/images/I/41WOalZ1N5L._AC_SY200_.jpg"
                         rating={4}
                     />
                     <Product 
+                        id={45671234}
                         title="Hairbrush"
                         price={15.98}
                         image="https://m.media-amazon.com/images/I/41yn8u3qJPL._AC_SY200_.jpg"
                         rating={2}
                     />
                     <Product 
+                        id={5678234}
                         title="Kenwood kMix Stand Mixer for Baking"
                         price={239.0}
                         rating={4}
@@ -47,7 +52,8 @@ function Home() {
                     />
                 </div>
                 <div className="home__row">
-                    <Product 
+                    <Product
+                        id={341234895}
                         title="Gaming Monitor"
                         price={598.99}
                         rating={4}
