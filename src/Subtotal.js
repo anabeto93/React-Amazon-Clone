1ece32477e84d7cf305d7ae6686ec162fd1967a6
Richard Anabeto Opoku <anabeto93@gmail.com>
2021-03-05 10:29:50 +0000

    setup part of checkout interface.

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -3,18 +3,10 @@ import "./Subtotal.css";
 import CurrencyFormat from "react-currency-format";
 import { useStateValue } from "./StateProvider";
 import { getBasketTotal } from "./reducer";
+import { Link, useHistory } from "react-router-dom";
 
 function Subtotal() {
-//   const getBasketTotal = (basket) => {
-//     let total = 0;
-
-//     for (const item in basket) {
-//       total += basket[item].price;
-//     }
-
-//     return total;
-//   };
-
+    const history = useHistory();
   const [{ basket }, dispatch] = useStateValue();
 
   return (
@@ -37,7 +29,7 @@ function Subtotal() {
         prefix={"$"}
       ></CurrencyFormat>
 
-      <button>Proceed to Checkout</button>
+      <button onClick={e => history.push('/payment')}>Proceed to Checkout</button>
     </div>
   );
 }
