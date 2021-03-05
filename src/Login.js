a4cf3bc502b1c7bda178c7febdc8b5975579bbfe
Richard Anabeto Opoku <anabeto93@gmail.com>
2021-03-05 06:34:43 +0000

    user authentication completed. Can login, logout and register a user simply with email and password.

--- src/Login.js
+++ src/Login.js
@@ -1,22 +1,39 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
+import { auth } from "./firebase";
 import "./Login.css";
 
 function Login() {
+  const history = useHistory();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   const signIn = (e) => {
-      e.preventDefault();
+    e.preventDefault();
 
-      //firebase logic to authenticate
+    //firebase logic to authenticate
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .then((auth) => {
+        history.push("/");
+      })
+      .catch((error) => alert(error.message));
   };
 
   const register = (e) => {
-      e.preventDefault();
+    e.preventDefault();
 
-      //fancy firebase registration
-  }
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((auth) => {
+        console.log(auth);
+
+        if (auth) {
+          history.push("/");
+        }
+      })
+      .catch((error) => alert(error.message));
+  };
 
   return (
     <div className="login">
--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,5 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase";
+
 const firebaseConfig = {
     apiKey: "AIzaSyDtpgC5MWuPYavG2QKpqdESR_LjQdgc72c",
     authDomain: "challenge-62d91.firebaseapp.com",
@@ -7,4 +8,11 @@ const firebaseConfig = {
     messagingSenderId: "391887136046",
     appId: "1:391887136046:web:c22a1c6666ecaad85946bf",
     measurementId: "G-1GWGF8MFYQ"
-  };
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
